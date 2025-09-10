9a8d0dab1027fcffcf47adcae0b0879a6b7edbd0
Bhairavi <bhairavisomannavar@gmail>
2025-09-10 10:01:27 +0530

    changes

---
+++ Task 3/app.js
@@ -0,0 +1,21 @@
+(function() {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+    equal.addEventListener('click', function(e) {
+        if (screen.value === '') {
+            screen.value = "Please enter";
+        } else {
+            try {
+                let answer = eval(screen.value);
+                screen.value = answer;
+            } catch (error) {
+                screen.value = "Error";
+            }
+        }
+    });
+    clear.addEventListener('click', function(e) {
+        screen.value = "";
+    });
+    })()
